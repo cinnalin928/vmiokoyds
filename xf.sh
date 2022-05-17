97588dfaf8b147186619149006ba5c2fca8148cb
cinnalin928 <105707634+cinnalin928@users.noreply.github.com>
2022-05-17 08:40:23 +0000

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "06dbf9cc-25ee-45cc-84f7-fca0fcc89366",
                         "alterId": 0
                     }
                 ],
